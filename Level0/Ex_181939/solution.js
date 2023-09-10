0dfc7615e2e28bc71237b464b2983adf3bcb71dc
Malg Eum Yu <134472216+yumalg12@users.noreply.github.com>
2023-09-10 22:25:17 +0900

    181939, 181941, 181942 개선

--- Level0/Ex_181939/solution.js
+++ Level0/Ex_181939/solution.js
@@ -1,5 +1,5 @@
 function solution(a, b) {
-    var ab = +(a+''+b);
-    var ba = +(b+''+a);
-    return (ab < ba)? ba: ab;;
+    var ab = +(`${a}${b}`);
+    var ba = +(`${b}${a}`);
+    return Math.max(ab,ba);
 }
--- Level0/Ex_181942/solution.js
+++ Level0/Ex_181942/solution.js
@@ -1,7 +1,5 @@
 function solution(str1, str2) {
     var answer = '';
-    for (var i=0; i < str1.length; i++){
-        answer += str1[i] + str2[i];
-    }
+    answer += [...str1].map((x, idx) => x+str2[idx]).join('');
     return answer;
 }
