9afc7cf25eac316e4bc7fd7fa006b6db985c8065
Malg Eum Yu <134472216+yumalg12@users.noreply.github.com>
2023-09-10 22:12:19 +0900

    181950, 181951

---
+++ Level0/Ex_181950/solution.js
@@ -0,0 +1,19 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = line.split(' ');
+}).on('close', function () {
+    str = input[0];
+    n = Number(input[1]);
+    t = "";
+    for (let i = 0; i < n; i++){
+        t += str;
+    }
+    console.log(t);
+});
